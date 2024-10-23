e230a4c20605e442e6a1fac8e97782e8f33512f7
Hineill <hineill.cespedes@pucp.edu.pe>
2024-10-23 11:15:22 -0500

    first commit

---
+++ src/main/java/com/example/proyectotemasavanzados/ProyectoTemasAvanzadosApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectotemasavanzados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoTemasAvanzadosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoTemasAvanzadosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/proyectotemasavanzados/ProyectoTemasAvanzadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectotemasavanzados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoTemasAvanzadosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
